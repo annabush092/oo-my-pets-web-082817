ba666a39a171f09c0708be4167465255eb1186d5
annabush092 <annabush092@gmail.com>
2017-08-31 15:33:11 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
 class Owner
-  # code goes here
+
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  ALL = []
+
+  def self.all
+    ALL
+  end
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    ALL << self
+  end
+
+  def self.reset_all
+    ALL.clear
+  end
+
+  def self.count
+    ALL.count
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.values.flatten.each do |pet|
+      pet.mood = "nervous"
+    end
+    pets.values.each do |pet_arr|
+      pet_arr.clear
+    end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+  end
+
 end
